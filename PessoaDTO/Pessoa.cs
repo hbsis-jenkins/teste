0843fada7091bdf44583ccce352822a9e1c86856
marcelo.sekimoto <marcelo.sekimoto>
2016-05-19 14:54:23 -0300

    start rep

---
+++ PessoaDTO/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PessoaDTO
+{
+    public class Pessoa
+    {
+        public String Cpf { get; set; }
+
+        public String Nome { get; set; }
+
+        public Pessoa(String cpf, string nome)
+        {
+            this.Cpf = cpf;
+            this.Nome = nome;
+        }
+    }
+}
---
+++ UnitTestPessoa/TesteUnitarioPessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PessoaDTO;
+
+namespace UnitTestePessoa
+{
+    [TestClass]
+    public class TesteUnitarioPessoa
+    {
+        [TestMethod]
+        public void Validar_Criacao_De_Uma_Instancia_De_Pessoa()
+        {
+            String strCPF = "11111111111", strNome = "Lorem Ipsum";
+
+            Pessoa clsPessoa = new Pessoa(strCPF, strNome);
+
+            Assert.IsNotNull(clsPessoa);
+            Assert.IsTrue(clsPessoa.Cpf.Equals(strCPF));
+            Assert.IsTrue(clsPessoa.Nome.Equals(strNome));
+        }
+    }
+}
